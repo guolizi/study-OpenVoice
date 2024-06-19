f9639ca24a421fcf10375a377579bf7268f9a1c1
guolizi <linbb8023@hotmail.com>
2024-06-19 17:05:53 +0800

    Update __init__.py

--- openvoice/text/__init__.py
+++ openvoice/text/__init__.py
@@ -17,7 +17,7 @@ def text_to_sequence(text, symbols, cleaner_names):
       List of integers corresponding to the symbols in the text
   '''
   sequence = []
-  symbol_to_id = {s: i for i, s in enumerate(symbols)}
+  symbol_to_id = {s: i for i, s in enumerate(symbols)}    # 读取音标到整数的映射表
   clean_text = _clean_text(text, cleaner_names)
   print(clean_text)
   print(f" length:{len(clean_text)}")
@@ -27,7 +27,7 @@ def text_to_sequence(text, symbols, cleaner_names):
     symbol_id = symbol_to_id[symbol]
     sequence += [symbol_id]
   print(f" length:{len(sequence)}")
-  return sequence
+  return sequence    # 转换成整数映射序列
 
 
 def cleaned_text_to_sequence(cleaned_text, symbols):
