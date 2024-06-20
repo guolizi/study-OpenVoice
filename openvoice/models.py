d84d8109ab6d06bd7679914b75bee1f387145c18
guolizi <linbb8023@hotmail.com>
2024-06-20 11:51:44 +0800

    Update models.py

--- openvoice/models.py
+++ openvoice/models.py
@@ -51,7 +51,7 @@ class TextEncoder(nn.Module):
 		self.proj= nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
 	def forward(self, x, x_lengths):
-		x = self.emb(x) * math.sqrt(self.hidden_channels) # [b, t, h]
+		x = self.emb(x) * math.sqrt(self.hidden_channels) # [b, t, h]	# 为什么要乘以这个，放大向量值？
 		x = torch.transpose(x, 1, -1) # [b, h, t]
 		x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x.size(2)), 1).to(x.dtype)
 
